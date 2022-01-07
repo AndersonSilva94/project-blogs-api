4597c3a5efb85d58b97a89090134a962048c2b79
Anderson Silva <andssilva.23@gmail.com>
2022-01-07 18:30:59 -0400

    feat: :rocket: cria a model de Categories

---
+++ models/Category.js
@@ -0,0 +1,16 @@
+const Category = (sequelize, DataTypes) => {
+  const CategoryModel = sequelize.define(
+    'Category',
+    {
+      name: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+      tableName: 'Categories',
+    },
+  );
+
+  return CategoryModel;
+};
+
+module.exports = Category;
