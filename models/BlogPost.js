9378199a5f13fe0c3c0d89158b792b7619513592
Anderson Silva <andssilva.23@gmail.com>
2022-01-07 18:50:40 -0400

    feat: :rocket: cria a model de BlogPost

---
+++ models/BlogPost.js
@@ -0,0 +1,23 @@
+const BlogPost = (sequelize, DataTypes) => {
+  const BlogPostModel = sequelize.define('BlogPost',
+    {
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+      published: DataTypes.DATE,
+      updated: DataTypes.DATE,
+    },
+    {
+      timestamps: false,
+      tableName: 'BlogPosts',
+    });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      foreignKey: 'id', as: 'userId',
+    });
+  };
+
+  return BlogPostModel;
+};
+
+module.exports = BlogPost;
