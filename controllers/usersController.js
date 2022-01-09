b1b5db9bf4b1f2916f8e38698db1cac3bdfe62d8
Anderson Silva <andssilva.23@gmail.com>
2022-01-09 04:04:18 -0400

    feat: :rocket: cria o função que busca todos os usuários no controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,7 +3,18 @@ const { userServices } = require('../services');
 const postUser = async (request, response, next) => {
   try {
     const validyUser = await userServices.createUser(request.body);
+
     return response.status(validyUser.status).json(validyUser.message);
+  } catch (err) {
+    return next(err);
+  }
+};
+
+const getAll = async (_request, response, next) => {
+  try {
+    const getUsers = await userServices.getAllUsers();
+
+    return response.status(getUsers.status).json(getUsers.message);
   } catch (err) {
     return next(err);
   }
@@ -11,4 +22,5 @@ const postUser = async (request, response, next) => {
 
 module.exports = {
   postUser,
+  getAll,
 };
