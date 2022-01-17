ae5055661972705a1158421bd143b84345b71ad1
Anderson Silva <andssilva.23@gmail.com>
2022-01-17 03:29:43 -0400

    feat(req 12): :rocket: cria o endpoint DELETE /user/me

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/', usersController.postUser);
 router.get('/', verifyToken, usersController.getAll);
 router.get('/:id', verifyToken, usersController.getById);
+router.delete('/me', verifyToken, usersController.deleteUser);
 
 module.exports = router;
